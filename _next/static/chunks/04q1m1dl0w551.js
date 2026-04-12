fa5e027fb58a227e1ed6dfeefa59a1d38c6a29b1
Kayk Mascarenhas <kaykdsg@gmail.com>
2026-04-12 18:34:41 -0300

    Updates

---
+++ _next/static/chunks/04q1m1dl0w551.js
@@ -0,0 +1,6 @@
+__turbopack_load_page_chunks__("/", [
+  "static/chunks/0v5_gvtzn-sy8.js",
+  "static/chunks/134p0b5hwo~tr.js",
+  "static/chunks/0v~g5_81ots--.js",
+  "static/chunks/turbopack-0sj4mot0z577v.js"
+])
