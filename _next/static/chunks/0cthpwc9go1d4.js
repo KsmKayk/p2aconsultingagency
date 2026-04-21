fab2a168d8e2a523ba81451393470df9ea27c65d
Kayk Mascarenhas <kaykdsg@gmail.com>
2026-04-21 00:59:21 -0300

    Updates

--- _next/static/chunks/0cthpwc9go1d4.js
+++ _next/static/chunks/0cthpwc9go1d4.js
@@ -1,8 +1,8 @@
 __turbopack_load_page_chunks__("/", [
-  "static/chunks/0ubx-ig_7hk2n.js",
+  "static/chunks/0j4hca419vdle.js",
   "static/chunks/0v~g5_81ots--.js",
+  "static/chunks/0g8gwpbsz06dq.js",
   "static/chunks/134p0b5hwo~tr.js",
-  "static/chunks/0jt7cyu2b8gwj.js",
   "static/chunks/0hwrndocxc5vc.css",
-  "static/chunks/turbopack-05qx~tuo7vbw~.js"
+  "static/chunks/turbopack-01~u3tbexhw6b.js"
 ])
--- _next/static/chunks/15ew1lva~ifdp.js
+++ _next/static/chunks/15ew1lva~ifdp.js
@@ -2,6 +2,6 @@ __turbopack_load_page_chunks__("/_app", [
   "static/chunks/0.-yjy92zu42-.js",
   "static/chunks/134p0b5hwo~tr.js",
   "static/chunks/0v~g5_81ots--.js",
-  "static/chunks/0jbgevhwfdoh4.css",
-  "static/chunks/turbopack-06p40d0v86znl.js"
+  "static/chunks/170qjxzv-hspu.css",
+  "static/chunks/turbopack-0_7xdoq05cq2o.js"
 ])
